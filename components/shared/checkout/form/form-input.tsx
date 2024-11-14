10c8153c552b6d511889cf0f3fca72294149390e
ViktorSorokin7500 <berhaneselassie1945@gmail.com>
2024-11-14 10:00:07 +0200

    added use client

--- components/shared/checkout/form/form-input.tsx
+++ components/shared/checkout/form/form-input.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Input } from "@/components/ui";
 import { ErrorText } from "./error-text";
 import { X } from "lucide-react";
--- components/shared/modal/auth-modal/forms/register-form.tsx
+++ components/shared/modal/auth-modal/forms/register-form.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
